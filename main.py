85a25b169aacfdfb956c78c3c7df5591fd9fb51a
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2024-04-21 20:18:57 +0200

    commit expressions

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 
-user_name = input("What is your name?:")
-print( "Welcome " + str(user_name) + "!")
+expression2 = (5 + 6) * 2
+print("expression2 is: " + str(expression2))
 
+expression3 = 6 * 4 - 10 / 5 * 2
+print("expression3 is: " + str(expression3))
+
+expression4 = ((15 + 5) - 10) / 2
+print("expression4 is: " + str(expression4))
