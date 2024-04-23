c5468dfd1dce7627b0cd72c46a39e3524805189a
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2024-04-23 06:39:59 +0200

    printing words backwards

--- main.py
+++ main.py
@@ -1,31 +1,15 @@
 
-word = "Eugene"
+# write a program that asks a user to enter a string of word then print the word backwards
 
-# Indexing from 0 to 5
+word = input("Enter a string of 6 character or a 6 letter word : ")
+
+# getting each character 
 char1 = word[0]
 char2 = word[1]
 char3 = word[2]
 char4 = word[3]
 char5 = word[4]
 char6 = word[5]
-print(char1)
-print(char2)
-print(char3)
-print(char4)
-print(char5)
-print(char6)
 
-print("~~~~~~~~~~~~~")
-
-char1 = word[-1]
-char2 = word[-2]
-char3 = word[-3]
-char4 = word[-4]
-char5 = word[-5]
-char6 = word[-6]
-print(char1)
-print(char2)
-print(char3)
-print(char4)
-print(char5)
-print(char6)
+print("Your word backward is : ")
+print(char6, char5, char4, char3, char2, char1)
