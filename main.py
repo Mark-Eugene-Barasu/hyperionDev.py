e403a9e544ad285bea104c4c08432a9dc615ef7a
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2024-04-23 20:30:30 +0200

    playing with string[start: excluded end: steps]

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 
 # Slicing multiple characters in a string.
 
-word = "Elephant"
-word2 = "Hippopotamus"
+print("Example 1:")
 
-print(word[2: 5]) # the second argument is exclusive 
-print(word2[5:10]) # the second argument is exclusive
-print("community"[3: : ])
-print("community"[::-1])
-print("xylophone"[::-2])
+veryLongWord = "supercalifragilisticexpialidocious"
+print(veryLongWord[0:5]) # super
+
+print("Example 2:")
+index = 6 # just a variable
+print(veryLongWord[index:9]) # ali
+
+print("Example 3:")
+print(veryLongWord[0::]) # supercalifragilisticexpialidocious
+print(veryLongWord[::]) # supercalifragilisticexpialidocious
+print(veryLongWord[:9]) # supercali
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 
-let user_Name = prompt(`Please what is your name?:`);
-console.log(`Hello ${user_Name}`);
+let randomNumbers = [5, 4 ,33 , 22 , 10, 1, 2 ,8]
+let words =  `Something something`;
+console.log(words.slice(4, 14)); // to print out a section selected
