ec7a5289365f623e89ecbabedeb3937fc3b1016d
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2024-04-23 06:58:22 +0200

    using the len() string method in Python

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
 
-# write a program that asks a user to enter a string of word then print the word backwards
-
-word = input("Enter a string of 6 character or a 6 letter word : ")
-
-# getting each character 
-char1 = word[0]
-char2 = word[1]
-char3 = word[2]
-char4 = word[3]
-char5 = word[4]
-char6 = word[5]
-
-print("Your word backward is : ")
-print(char6, char5, char4, char3, char2, char1)
+# using len() string method
+oldName = "Kofi"
+myName = input(f"type in any name like {oldName} : ")
+print(f"{myName} has {len(myName)} character")
