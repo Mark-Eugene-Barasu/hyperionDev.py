88d2af453f8e040312bdcd1029e5e3ea5a0fd735
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2024-04-23 05:11:58 +0200

    salary calculator

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 
-number = int(input("Please enter a number"))
-answer = number * 10
-print(str(number) + " x 10 = " + str(answer))
+#  salary calculator 
+noDays = int(input("How many days did you work this month? : "))
+payPerDay = float(input("How much is your pay per day? : "))
+salary = noDays * payPerDay
+print(str(f"Your salary for the month is R{salary}"))
