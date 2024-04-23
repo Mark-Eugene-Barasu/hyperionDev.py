6c615d8127d89fe74aef50055bfe22ded4553fae
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2024-04-23 07:20:49 +0200

    slicing string[start: end: steps]

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 
-# using len() string method
-oldName = "Kofi"
-myName = input(f"type in any name like {oldName} : ")
-print(f"{myName} has {len(myName)} character")
+# Slicing multiple characters in a string.
+
+word = "Elephant"
+word2 = "Hippopotamus"
+
+print(word[2: 5]) # the second argument is exclusive 
+print(word2[5:10]) # the second argument is exclusive
+print("community"[3: : ])
+print("community"[::-1])
+print("xylophone"[::-2])
