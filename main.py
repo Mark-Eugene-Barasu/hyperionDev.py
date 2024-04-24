02c79b1c24801ef7749e72065e5fd91f5796556d
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2024-04-24 03:15:45 +0200

    PseudoCode and Flowcharts

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-# testing 
-sentence = "I!am!a!coding!genius"
-sentence = sentence.replace("!", " ")
-print(sentence)
-print(sentence[::-1])
+
+# PseudoCode & Flowcharts
+# get a integer number input from user
+# if integer is even
+#   print "fizz"
+# else:
+#   print "buzz"
+
+# Actual Code
+integer = int(input("Enter any integer number : "))
+if integer%2 == 0:
+    print("fizz")
+else:
+    print("buzz")
+
