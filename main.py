13a2a0034d7a0088c599f3c428a2a866367a85f9
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2024-04-24 19:44:11 +0200

    I enjoyed this while loop assignment, I rocked my mind a bit

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-# asking for user name and email
-userName = input("Enter your name : ")
-userEmail = input("Enter your email address : ")
 
-# outputting a message with the user info
-print(f"Hi {userName}! \nWe will be contacting you shortly at {userEmail}")
+# initializing and assigning variables
+keep = 0 # values from input
+adding = 0 # for adding of inputs
+count = 0 # iterate each loop 
+
+while keep != -1: # (0 != -1) == true just to kick start loop
+    count += 1 # increment by 1
+    keep = float(input("enter number please : ")) # get input
+    adding += keep # add every input
+
+adding += 1 # plus one to counter oppose the last number of -1 to end the loop
+count -= 1 # deducted one to exclude the last count with the value -1
+average = adding / count # calculate average
+print(f"average is {average}") #print average
+
+# I really enjoyed this assignment. It really rocked my brain a bit :)
