3a4dab4ea26066187abbbfce6c3cff9fd5dcaead
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2024-04-24 02:10:17 +0200

    using .upper() and .lower() string methods to manipulate a string

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-# This is an example Python program.
-# The user inputs a number.
-# The program then outputs all the EVEN numbers from 0 to that number.
-rangeNum = int(input("Enter the max number you'd like to go up to: \n"))
-
-# We define a for loop that runs from 0 to rangeNum.
-for i in range (0, rangeNum):          
-  # This checks if the current number of the loop is EVEN.
-  if i%2 == 0: 
-    print(i)
+# More String Methods
+aString = "Hello World"
+print(aString.upper())
+print(aString.lower())
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
-let randomNumbers = [5, 4 ,33 , 22 , 10, 1, 2 ,8]
-let words =  `Something something`;
-console.log(words.slice(4, 14)); // to print out a section selected
+// More String Methods
+let aString = "Hello World";
+console.log(aString.toUpperCase());
+console.log(aString.toLowerCase())
