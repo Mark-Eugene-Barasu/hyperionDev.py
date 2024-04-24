2814b002bc15dcef8dc381cabd9417bb7fc6fd48
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2024-04-24 02:25:29 +0200

    Done the following test perfectly

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-# Using the "string".strip()
-strHelp = "*********Please leave me alone***********"
-print(strHelp.strip("*"))
+# testing 
+sentence = "I!am!a!coding!genius"
+sentence = sentence.replace("!", " ")
+print(sentence)
+print(sentence[::-1])
