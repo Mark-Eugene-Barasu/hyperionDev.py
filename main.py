abd0b3ef8c2190b439b742cf2cc57b4b1b4386e4
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2024-04-24 20:32:58 +0200

    If, Elif, and Else statements

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 
-# initializing and assigning variables
-keep = 0 # values from input
-adding = 0 # for adding of inputs
-count = 0 # iterate each loop 
+# if statement
 
-while keep != -1: # (0 != -1) == true just to kick start loop
-    count += 1 # increment by 1
-    keep = float(input("enter number please : ")) # get input
-    adding += keep # add every input
+current_hour = 22
+guards_presence = False
 
-adding += 1 # plus one to counter oppose the last number of -1 to end the loop
-count -= 1 # deducted one to exclude the last count with the value -1
-average = adding / count # calculate average
-print(f"average is {average}") #print average
+if ((current_hour >= 7) and (current_hour <= 17)):
+    print("You're in")
+elif ((current_hour < 7 and guards_presence == True) or (current_hour > 17 and guards_presence == True)):
+    print("You're in")
+else:
+    print("Sorry, Try another day.")
 
-# I really enjoyed this assignment. It really rocked my brain a bit :)
+
+# great assignment on if elif and else statement
