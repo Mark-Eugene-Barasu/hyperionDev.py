6db1d1e3bb4e95d277990c711afe9bf0f22b0272
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2024-04-24 04:46:52 +0200

    list.index(anyElement) versus list[i]

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
 
-# Lists (also known as Array from JavaScript)
+# List methods
 groceries = ["apples", "milk", "cheese", "bread"]
-print(groceries)
-# print(type(groceries))
-# print(isinstance(groceries, list))
-
-
-print(groceries[0]) # 1st list item
-print(groceries[1]) # 2nd list item
-print(groceries[2]) # 3rd list item
-print(groceries[3]) # 4th list item
-
+print(groceries.index("apples"))
+print(groceries[0])
