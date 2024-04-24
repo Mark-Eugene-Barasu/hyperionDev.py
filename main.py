ac8090789792106f78810b0bc2af42e549062121
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2024-04-24 03:46:42 +0200

    while loop in python

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
 
-# If statements
-num = 10
-if (num < 12):
-    print("the variable num is lower than 12")
-elif (num > 12):
-    print("the variable num is greater than 12")
-else:
-    print("The variable num is equal to 12")
-
-# actual code for previous PseudoCode
-integer = int(input("Enter an integer : "))
-if (integer%2 == 0):
-    print("fizz")
-else: 
-    print("buzz")
+# While Loops
+num = 1
+while (num < 11):
+    print(num)
+    num += 1
