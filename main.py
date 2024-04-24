d31e843992e320fb459097502cffff79ff86d12d
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2024-04-24 05:49:39 +0200

    list.append('coffee') == list.insert(len(list), 'coffee') // true

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 
 # List methods
 groceries = ["apples", "milk", "cheese", "bread"]
-groceries.append("coffee") # ["apples", "milk", "cheese", "bread"]
-groceries.insert(0, "carrots") # ["carrots", "apples", "milk", "cheese", "bread", "coffee"]
-groceries.insert(1, "peas") # ["carrots", "peas", "apples", "milk", "cheese", "bread", "coffee"]
-groceries.pop() # ["carrots", "peas", "apples", "milk", "cheese", "bread"]
-groceries.pop(0) # ["peas", "apples", "milk","cheese", "bread"]
-groceries.remove("cheese") # ["peas","apples", "milk", "bread"]
+# groceries.append("coffee") # ["apples", "milk", "cheese", "bread"]
+# groceries.insert(len(groceries), "carrots") #["apples", "milk", "cheese", "bread", "coffee"]
 print(groceries)
+print(groceries.append("coffee") == groceries.insert(len(groceries), "coffee"))
+
